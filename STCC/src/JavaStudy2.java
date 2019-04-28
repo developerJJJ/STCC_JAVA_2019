397c1c5482a8377d127bdfdce93dabf5178518ac
JJJ <JJJ@61.99.101.72>
2019-04-28 18:52:28 +0900

    STCC Java Study

---
+++ STCC/src/JavaStudy2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // Scanner 패키지 import
+
+public class JavaStudy2 { //JavaStudy 클래스	
+	public static void main(String[] args) { // main 메소드
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String n;
+		int i;
+		
+		System.out.println("name?");
+		n = sc.nextLine(); // 공백 이전까지의 문자열을 입력 받음 공백이 필요하면 nextLine()
+		System.out.println(n);
+		
+		System.out.println("number?");
+		i = sc.nextInt(); // next+자료형()
+		System.out.println(i);		
+	}
+}
